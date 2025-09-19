a48358c9f0d0ce89583223743f5a87ce4478fb4c
Okaa <owkaa@outlook.dk>
2025-09-19 12:33:33 +0200

    daw

--- python/scipycg.py
+++ python/scipycg.py
@@ -1,18 +1,19 @@
 import numpy as np
-from scipy.sparse import csc_array
 import scipy.sparse.linalg as sp
 import linearSolver as ls
 from scipy.io import mmread
 import Precondition as ps
 
 
-A = mmread("matrixData/bcsstk14.mtx.gz").toarray()
+A = mmread("matrixData/nos1.mtx.gz").toarray()
 size = np.shape(A)[0]
 b = np.ones((size, 1))
 M_inv = ps.Jacobi(A)
-x, exit_code = sp.bicgstab(A, b)
+x = np.zeros((size,1))
+x, exit_code = sp.bicgstab(A, b, M = np.eye(size),maxiter = 1000000)
 print(exit_code)
 print(np.allclose(A.dot(x), b))
+print(np.linalg.norm(A.dot(x)-b))
+print()
 
-x = ls.BiCGSTAB(A,b)
-print(np.allclose(A.dot(x), b))
+x = ls.CG(A, b, M_inv=M_inv, verbose = True)
