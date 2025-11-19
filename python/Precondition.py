b6e05c9fdde87b7a80ede7ea60318368be4f56f6
Okaa <owkaa@outlook.dk>
2025-11-19 11:26:49 +0100

    d

--- python/Precondition.py
+++ python/Precondition.py
@@ -55,35 +55,18 @@ def shift(size, numOffDiag = 1, rng = None, upper = True):
         if numOffDiag < size:
             numOffDiag = range(1, numOffDiag + 1)
     
-    # alpha = rng.normal(size=size)
-    # beta = rng.normal(scale=0.1,size=size-1)* rng.binomial(1,0.5, size=size-1)
+    scale = 1
 
-    # beta2 = rng.normal(scale=0.05,size=size-2)* rng.binomial(1,0.25, size=size-2)
-    # gamma = rng.normal()
-
-    # M = np.diag(alpha)
     M = np.eye(size)
-    # M = np.diag(rng.uniform(size=size))
-    # M += np.diag(beta,k=1)
-    # M += np.diag(beta,k=-1)
 
-    # M += np.diag(beta2,k=-2)
-    # M += np.diag(beta2,k=2)
-    # # M[size-1,0] = gamma
-    # M[0,size-1] = gamma
     bound = 0.01
-    # zeroed = rng.uniform(high=0.1)
-    # print(zeroed)
-    # print(zeroed)
-    scale = 0.005
     for i in numOffDiag:
-        offDiag = rng.normal(scale=scale, size=size-i) #* rng.binomial(1,0.007, size=size-i)
+        offDiag = rng.normal(scale=scale, size=size-i) #* rng.binomial(1,0.5, size=size-i)
         # offDiag = rng.uniform(low=-bound, high=bound,size=size-i)#*rng.binomial(1,0.1, size=size-i)
-        # if upper:
-        M += np.diag(offDiag, k = i)
-        # else:
-            # M += np.diag(offDiag, k = -i)
-        # M[0,size-1] = rng.normal(scale=scale)
+        if upper:
+            M += np.diag(offDiag, k = i)
+        else:
+            M += np.diag(offDiag, k = -i)
 
     return spar.csr_array(M)
 
--- python/shiftTest.py
+++ python/shiftTest.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 
 # A = mmread("matrixData/bcsstk01.mtx.gz")
 A = mmread("matrixData/bcspwr03.mtx.gz")
+print(A)
 size = np.shape(A)[0]
 
 b = np.ones((size, 1))
@@ -17,13 +18,15 @@ b = np.ones((size, 1))
 _, r_norm_nonPre , nonPreK = ls.BiCGSTAB(A, b, verbose = True)
 
 smallestK = 10000
+largestK = 0
 smallestSeed = -1
+largestSeed = -1
 
 numOffDiag = 2
-numPrecond = 2
+numPrecond = 10
 k_list = []
 
-
+maxIter = 400
 for i in range(1000):
     print(i, end = '\r')
     
@@ -35,11 +38,14 @@ for i in range(1000):
     #     print(i,np.linalg.eigvals(M_inv.toarray()))
     #     # break
     # print((np.linalg.det(M_inv)))
-    _, _, k = ls.BiCGSTAB(A, b, M_inv = M_inv, verbose = False)
+    _, _, k = ls.BiCGSTAB(A, b, M_inv = M_inv, verbose = False, maxIter= maxIter)
     k_list.append(k)
     if k < smallestK:
         smallestSeed = i
         smallestK = k
+    if k > largestK:
+        largestK = k
+        largestSeed = i
     # if k < nonPreK:
     #     print(i)
     #     break
@@ -49,20 +55,28 @@ print(smallestK, smallestSeed)
 
 rng = np.random.default_rng(smallestSeed)
 M_inv = prec.multiShift(size, numShift = numPrecond, numOffDiag = numOffDiag, rng = rng)
+
+rng = np.random.default_rng(largestSeed)
+M_inv_bad = prec.multiShift(size, numShift = numPrecond, numOffDiag = numOffDiag, rng = rng)
 # print(M_inv)
 # print(np.linalg.eigvals(M_inv.toarray()))
-# print(np.linalg.cond(A.toarray()),np.linalg.cond(M_inv@A))
+print("A:",np.linalg.cond(A.toarray()),"\nM_inv@A:",np.linalg.cond(M_inv@A.toarray()),"\nM_inv_bad@A:",np.linalg.cond(M_inv_bad@A.toarray()), "\n")
 
-_, r_norm_pre, k = ls.BiCGSTAB(A, b, M_inv = M_inv, verbose = True)
+_, r_norm_pre, k = ls.BiCGSTAB(A, b, M_inv = M_inv, verbose = True, maxIter= maxIter)
+_, r_norm_pre_bad, k = ls.BiCGSTAB(A, b, M_inv = M_inv_bad, verbose = True, maxIter= maxIter)
 plt.figure(1)
-plt.hist(k_list, bins=25)
+plt.hist(k_list, bins = 25)
 plt.title(f"Min: {np.min(k_list)}, mean: {np.mean(k_list)}, max: {np.max(k_list)}")
 
 plt.figure(2)
 plt.plot((range(len(r_norm_nonPre[0]))), np.log(r_norm_nonPre[0]), label = "Non Precondtioned r")
 plt.plot((range(len(r_norm_nonPre[1]))), np.log(r_norm_nonPre[1]), label = "Non Precondtioned s")
-plt.plot((range(len(r_norm_pre[0]))), np.log(r_norm_pre[0]), label = "Precondtioned r")
-plt.plot((range(len(r_norm_pre[1]))), np.log(r_norm_pre[1]), label = "Precondtioned s")
+
+plt.plot((range(len(r_norm_pre[0]))), np.log(r_norm_pre[0]), label = "Best Precondtioned r")
+plt.plot((range(len(r_norm_pre[1]))), np.log(r_norm_pre[1]), label = "Best Precondtioned s")
+
+plt.plot((range(len(r_norm_pre_bad[0]))), np.log(r_norm_pre_bad[0]), label = "Worst Precondtioned r")
+plt.plot((range(len(r_norm_pre_bad[1]))), np.log(r_norm_pre_bad[1]), label = "Worst Precondtioned s")
 plt.legend()
 plt.title("Log Residual norm")
 plt.ylabel("log residual norm")
